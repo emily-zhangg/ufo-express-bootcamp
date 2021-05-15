f519450a5abe9bd2dca14529fd6939e9de040ce1
Yiqing Zhang <emily@officecleanz.com>
2021-05-15 19:02:16 +0800

    comfortable

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import express from 'express';
-import { add,read, write } from './jsonFileStorage.js';
 import methodOverride from 'method-override';
+import { add, read, write } from './jsonFileStorage.js';
 
 const app = express();
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
 const handleSightingFormRequest = (req, res) => {
+  let invalidInput = false;
+  for (let i = 0; i < Object.values(req.body).length; i += 1) {
+    if (Object.values(req.body)[i] === '') {
+      invalidInput = true;
+    }
+  }
+  const timeSubmitted = new Date();
+  req.body.dateCreated = timeSubmitted.toISOString().slice(0, 10);
   add('data.json', 'sightings', req.body, (err) => { console.log(err); });
-  res.redirect('/sightingForm');
+  if (invalidInput === true) { res.send('Please do not leave any field empty'); } else { res.redirect('/sightingForm'); }
 };
 
 app.get('/sightingForm', (req, res) => {
@@ -55,29 +63,28 @@ app.get('/shape', (req, res) => {
     res.render('shapes', data);
   });
 });
-app.get('/sighting/:index/edit',(req,res)=>{
-  read('data.json',(err,content)=>{
-    const data = {index:req.params.index,data:content.sightings[req.params.index]}
-    res.render('sightingsEditForm',data)
-  })
-})
-app.put('/sighting/:index/edit',(req,res)=>{
-  read('data.json',(err,content)=>{
-    content.sightings[req.params.index]=req.body;
-    console.log(req.body)
-    write('data.json',content,(err,content)=>{
-    })
-    res.render('notif',{message:'You have successfully edited the UFO entry'})
-  })
-
-})
-app.delete('/:index',(req,res)=>{
-  read('data.json',(err,content)=>{
-    content.sightings.splice(Number(req.params.index),1)
-    write('data.json',content,(err,content)=>{
-    })
-    res.render('notif',{message:'You have successfully deleted the UFO entry'})
-  })
-})
+app.get('/sighting/:index/edit', (req, res) => {
+  read('data.json', (err, content) => {
+    const data = { index: req.params.index, data: content.sightings[req.params.index] };
+    res.render('sightingsEditForm', data);
+  });
+});
+app.put('/sighting/:index/edit', (req, res) => {
+  read('data.json', (err, content) => {
+    content.sightings[req.params.index] = req.body;
+    console.log(req.body);
+    write('data.json', content, (err, content) => {
+    });
+    res.render('notif', { message: 'You have successfully edited the UFO entry' });
+  });
+});
+app.delete('/:index', (req, res) => {
+  read('data.json', (err, content) => {
+    content.sightings.splice(Number(req.params.index), 1);
+    write('data.json', content, (err, content) => {
+    });
+    res.render('notif', { message: 'You have successfully deleted the UFO entry' });
+  });
+});
 app.listen(3004);
 console.log('https://localhost:3004');
